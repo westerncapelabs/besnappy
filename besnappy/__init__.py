3890f6f17b7367a255b9c4118925b48bb4096086
Mike Jones <mike@westerncapelabs.com>
2014-09-25 15:41:53 +0200

    Bump version to 0.1.0a post-release.

--- besnappy/__init__.py
+++ besnappy/__init__.py
@@ -1,6 +1,6 @@
 """An unofficial client library for the BeSnappy HTTP API.."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.0a"
 
 from .tickets import SnappyApiSender
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="besnappy",
-    version="0.1.0",
+    version="0.1.0a",
     url='http://github.com/westerncapelabs/besnappy',
     license='BSD',
     description="An unofficial client library for the BeSnappy HTTP API.",
